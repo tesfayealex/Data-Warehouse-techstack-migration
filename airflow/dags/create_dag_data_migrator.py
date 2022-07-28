114808db77ed5cb0b8540f1c31889a71cefee028
tesfayealex <tesfayealemayehu27@gmail.com>
2022-07-28 23:15:59 +0300

    tried some ways to migrate data

--- airflow/dags/create_dag_data_migrator.py
+++ airflow/dags/create_dag_data_migrator.py
@@ -7,51 +7,51 @@ import logging
 log: logging.log = logging.getLogger("airflow")
 log.setLevel(logging.INFO)
 
-default_args={
-    'owner':'tesfaye',
-    'retries':5,
-    'retry_delay':timedelta(minutes=2)
-}
+# default_args={
+#     'owner':'tesfaye',
+#     'retries':5,
+#     'retry_delay':timedelta(minutes=2)
+# }
 
-def get_table_lists(database):
-    host_name="localhost"
-    db_user="admin"
-    db_password="admin"
-    db_name="trial"
+# def get_table_lists(database):
+#     host_name="localhost"
+#     db_user="admin"
+#     db_password="admin"
+#     db_name="trial"
 
-    connection = db_connect.connect(host=host_name,user=db_user,password=db_password,database=db_name)
-    cursor = connection.cursor()
-    query = "SELECT table_schema FROM information_schema.tables WHERE table_schema != 'pg_catalog' and table_schema != 'information_schema' ORDER BY table_schema"
-    cursor.execute(query)
-    schemas = cursor.fetchall()
-    print(schemas)
-    new_schemas = list(set(schemas))
-    print(new_schemas)
+#     connection = db_connect.connect(host=host_name,user=db_user,password=db_password,database=db_name)
+#     cursor = connection.cursor()
+#     query = "SELECT table_schema FROM information_schema.tables WHERE table_schema != 'pg_catalog' and table_schema != 'information_schema' ORDER BY table_schema"
+#     cursor.execute(query)
+#     schemas = cursor.fetchall()
+#     print(schemas)
+#     new_schemas = list(set(schemas))
+#     print(new_schemas)
 
-with DAG(
-    dag_id='load_data',
-    default_args=default_args,
-    description='extract and load raw data from the given dataset',
-    start_date=datetime(2022,7,6,2),
-    schedule_interval='@once'
-)as dag:
-    task1 = PostgresOperator(
-        task_id='create_warehouse_database',
-        postgres_conn_id='postgres_connection',
-        sql='/sql/create_raw_data.sql',
-    )
-    task2 = PostgresOperator(
-        task_id='create_dataset_table',
-        postgres_conn_id='postgres_connection',
-        sql='/sql/create_raw_data.sql',
-    )
-    task3 = PostgresOperator(
-        task_id='load_dataset',
-        postgres_conn_id='postgres_connection',
-        sql='/sql/load_raw_data.sql',
-    )
+# with DAG(
+#     dag_id='load_data',
+#     default_args=default_args,
+#     description='extract and load raw data from the given dataset',
+#     start_date=datetime(2022,7,6,2),
+#     schedule_interval='@once'
+# )as dag:
+#     task1 = PostgresOperator(
+#         task_id='create_warehouse_database',
+#         postgres_conn_id='postgres_connection',
+#         sql='/sql/create_raw_data.sql',
+#     )
+#     task2 = PostgresOperator(
+#         task_id='create_dataset_table',
+#         postgres_conn_id='postgres_connection',
+#         sql='/sql/create_raw_data.sql',
+#     )
+#     task3 = PostgresOperator(
+#         task_id='load_dataset',
+#         postgres_conn_id='postgres_connection',
+#         sql='/sql/load_raw_data.sql',
+#     )
 
-    task1 >> task2 >> task3
+#     task1 >> task2 >> task3
 
 
 import subprocess
@@ -121,3 +121,48 @@ for s in schemas:
        # conn = pymysql.connect("localhost", "admin", "admin")
        # print(conn)
        break;
+
+     
+query = "pg_dump mydb > db.sql"
+
+
+cursor.execute(query)
+# schemas = cursor.fetchall()
+print(cursor.fetchall())
+
+try:
+            process = subprocess.Popen(
+                ['pg_dump',
+                 '--dbname=postgresql://{}:{}@{}:{}/{}'.format("admin", 'admin', "localhost", "5432", "trial"),
+                 '-Fc',
+                 '-f', "db.sql",
+                 '-v'],
+                stdout=subprocess.PIPE
+            )
+            output = process.communicate()[0]
+            if int(process.returncode) != 0:
+                print('Command failed. Return code : {}'.format(process.returncode))
+                exit(1)
+            print(output)
+except Exception as e:
+            print(e)
+            exit(1)
+
+import pymysql
+
+conn = pymysql.connect("localhost", "admin", "admin")
+print(conn)
+import file
+proc = subprocess.Popen(["mysql", "--user=%s" % "admin", "--password=%s" % "admin", "trial"],
+                        stdin=subprocess.PIPE,
+                        stdout=subprocess.PIPE)
+f = open("./db.sql", "r")
+out, err = proc.communicate(f.read())
+print(out)
+print(err)
+
+with open('db.sql', 'r') as f: 
+       command = ["mysql", "--user=%s" % "admin", "--password=%s" % "admin", "trial"]
+       proc = subprocess.Popen(command, stdin = f)
+       stdout, stderr = proc.communicate()
+       print(stdout)
