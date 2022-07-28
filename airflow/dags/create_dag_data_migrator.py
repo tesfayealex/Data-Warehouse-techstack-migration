051444bec3f15e35de7e243daaadc36f837567cf
tesfayealex <tesfayealemayehu27@gmail.com>
2022-07-28 23:13:25 +0300

    added connections and data type converters

--- airflow/dags/create_dag_data_migrator.py
+++ airflow/dags/create_dag_data_migrator.py
@@ -52,3 +52,49 @@ with DAG(
     )
 
     task1 >> task2 >> task3
+
+
+import subprocess
+host_name="localhost"
+db_user="admin"
+db_password="admin"
+db_name="trial"
+
+connection = db_connect.connect(host=host_name,user=db_user,password=db_password,database=db_name)
+    
+cursor = connection.cursor()
+query = "SELECT table_schema,table_name FROM information_schema.tables WHERE table_schema != 'pg_catalog' and table_schema != 'information_schema' ORDER BY table_schema"
+cursor.execute(query)
+schemas = cursor.fetchall()
+print(schemas)
+new_schemas = list(set(schemas))
+print(new_schemas)
+
+def convert_type(type):
+       if  type == "boolean":
+              return "bit(1)"
+       elif type == "integer":
+              return  "int"
+       elif type == "double precision":
+              return "double"
+       elif type == "character varying":
+              return "text"
+       else:
+              return type
+       #  func=switcher.get(type,lambda : type)
+       #  return func
+
+def create_table_query(table,columns):
+       create_query = f"CREATE TABLE IF NOT EXISTS {table[1]}("
+       for index , c in enumerate(columns):
+        data_type = convert_type(c[2])
+        print(data_type)
+        nullable = "NULL" if c[1] == "YES" else "NOT NULL"
+        if index != 0:
+              create_query += ','
+        create_query +=   "`" +  str(c[0]) + "`" + " "  + str(data_type) + " " + str(nullable)
+       create_query += ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;"
+       return create_query
+
+def create_db_query(name):
+       db_query = f"CREATE DATABSE IF NOT EXISTS {name}"
