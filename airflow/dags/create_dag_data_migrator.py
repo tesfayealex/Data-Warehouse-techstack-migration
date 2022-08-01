9305f58ac49d6be339b41241959837b63319ff20
tesfayealex <tesfayealemayehu27@gmail.com>
2022-08-01 15:00:49 +0300

    some cleanups

--- airflow/dags/create_dag_data_migrator.py
+++ airflow/dags/create_dag_data_migrator.py
@@ -9,7 +9,7 @@ import pandas as pd
 host_name="localhost"
 db_user="admin"
 db_password="admin"
-# db_name="trial"
+db_name="trial"
 
 default_args={
     'owner':'tesfaye',
@@ -45,7 +45,6 @@ def create_table_query(table,columns):
        create_query = f"USE {table[0]}; CREATE TABLE IF NOT EXISTS {table[1]}("
        for index , c in enumerate(columns):
         data_type = convert_type(c[2])
-        print(data_type)
         nullable = "NULL" if c[1] == "YES" else "NOT NULL"
         if index != 0:
               create_query += ','
@@ -66,43 +65,30 @@ def create_schemas_and_load_data(database_name , schemas):
        postgres_engine = create_engine(f'postgresql+psycopg2://{db_user}:{db_password}@{host_name}/{database_name}')
        cursor = connection.cursor()
        for s in schemas:
-              if s[0] == "public":
                      query = f"SELECT column_name , is_nullable , data_type  FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA= '{str(s[0])}' and TABLE_NAME = '{str(s[1])}'"
-                     print(query)
                      cursor.execute(query)
                      columns = cursor.fetchall()
-                     print (columns)
                      mysql_connection = f'mysql://admin:admin@127.0.0.1:3306/{database_name}'
                      engine = create_engine(mysql_connection)
                      conn = engine.connect()
                      db_query  = create_db_query(s[0])
-                     print(db_query)
                      db = conn.execute(db_query)
                      singe_db_connection = f'mysql://admin:admin@127.0.0.1:3306/{s[0]}'
                      single_db_engine = create_engine(singe_db_connection)
                      single_db_conn = engine.connect()
-                     # print(db.fetchall())
                      create_query = create_table_query(s,columns)
-                     print(create_query)
                      conn.execute(create_query)
-                     # print(cursor.fetchall())
                      the_data = pd.read_sql(f'SELECT * FROM {s[0]}.{s[1]}',postgres_engine)
-                     # query = text(f'show tables')
-                     print(the_data)
                      x = the_data.to_sql(s[1], con=single_db_engine, if_exists='replace', index=False)
-                     print(x)
                      get_data_query = f'select * from {s[0]}.{s[1]};'
 
 def migrate_privilages(database_name):
        connection = db_connect.connect(host=host_name,user=db_user,password=db_password,database=database_name)
        cursor = connection.cursor()
        query = f"SELECT * FROM information_schema.table_privileges"
-       print(query)
        cursor.execute(query)
        columns = cursor.fetchall()
-       print(columns)
        for c in columns:
-              if c[1] == 'try_user':
                      check_user_existance = f'SELECT user,host FROM mysql.user where user = "{c[1]}"'
                      query = f'GRANT {c[5]} on {c[3]}.{c[4]} to {c[1]}@`localhost`;' 
                      mysql_connection = f'mysql://{db_user}:{db_password}@{host_name}:3306/{c[3]}'
@@ -112,7 +98,6 @@ def migrate_privilages(database_name):
                      if len(excuted.fetchall()) == 0:
                             create_user_query = f'CREATE USER {c[1]}@`{host_name}` IDENTIFIED WITH mysql_native_password BY "password";'
                             excuted = conn.execute(create_user_query)
-                     print(query)
                      if c[5] != "TRUNCATE":
                             excuted = conn.execute(query)
 
